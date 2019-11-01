e4f7e16ffa627c2e7394688c45cf5846c6cefd51
@seera.kaenkaew8 <seera.kaenkaew8@gmail.com>
2019-11-01 13:41:28 +0700

    Initial commit

---
+++ src/views/TodosList.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">{{todo.text}}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        { id: "1", text: "List 1" },
+        { id: "2", text: "List 2" },
+        { id: "3", text: "List 3" },
+        { id: "4", text: "List 4" },
+        { id: "5", text: "List 5" }
+      ]
+    };
+  }
+};
+</script>
+
